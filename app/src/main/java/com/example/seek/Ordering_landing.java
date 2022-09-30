c981afaab78608aa59fc2c0ad801aee426b91609
DivSi06 <singhaldivyanshi42@gmail.com>
2022-09-30 14:17:39 +0530

    lazeez link

--- app/src/main/java/com/example/seek/Ordering_landing.java
+++ app/src/main/java/com/example/seek/Ordering_landing.java
@@ -2,13 +2,33 @@ package com.example.seek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Ordering_landing extends AppCompatActivity {
 
+    ImageButton buttonLazeez;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ordering_landing);
+
+        buttonLazeez = (ImageButton) findViewById(R.id.lazeezButton);
+
+        buttonLazeez.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLazeez();
+            }
+        });
+
+
+    }
+    public void openLazeez(){
+        Intent intent =new Intent(this, Lazeez.class);
+        startActivity(intent);
     }
 }
